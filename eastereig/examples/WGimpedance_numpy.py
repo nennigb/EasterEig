0d3690b736730aa2f89ea6d45e32312929e6275d
bn <benoit.nennig@isae-supmeca.fr>
2023-03-08 16:40:26 +0100

    Lint `WGimpedance_numpy.py`.

--- eastereig/examples/WGimpedance_numpy.py
+++ eastereig/examples/WGimpedance_numpy.py
@@ -23,7 +23,7 @@ numpy version.
 
 ```
    y=h ____________________________ rigid wall / Neumann bc
-   
+
            | |
           -|-|-->     oo-duct
            | |
@@ -33,7 +33,7 @@ numpy version.
 ```
 
 Description
-------------    
+------------
 This problem is described in sec. 4.1 of arxiv.org/abs/1909.11579
 and yield a **generalized eigenvalue problem** and the eigenvalue lda stands for the axial wavenumber
 
@@ -78,13 +78,12 @@ import matplotlib.pyplot as plt
 import eastereig as ee
 
 
-
 class Znumpy(ee.OP):
     """Create a subclass of the interface class OP that describe the problem operator."""
 
-    def __init__(self,z,n,h,rho,c,k):
+    def __init__(self, z, n, h, rho, c, k):
         """Initialize the problem.
-        
+
         Parameters
         ----------
             z : complex
@@ -100,66 +99,63 @@ class Znumpy(ee.OP):
             k : float
                 air wavenumber
         """
-
-        self.h=float(h)
-        self.n=n
+        self.h = float(h)
+        self.n = n
         # air properties
-        self.rho,self.c,self.k = rho, c, k
+        self.rho, self.c, self.k = rho, c, k
         # element length
-        self.Le= h/(n-1)
+        self.Le = h/(n-1)
 
         # assemble *base* matrices, independant of the parameter
-        self._mass() # create _Mmat
-        self._stif() # create _Kmat
+        self._mass()  # create _Mmat
+        self._stif()  # create _Kmat
         self._gam()  # create _GamMat
         # initialize OP interface
         self.setnu0(z)
-        
+
         # mandatory -----------------------------------------------------------
-        self._lib='numpy'
+        self._lib = 'numpy'
         # create the operator matrices
-        self.K=self._ImpMat()
+        self.K = self._ImpMat()
         # define the list of function to compute  the derivatives of each operator matrix
-        self.dK = [self._dstiff, self._dmass]        
+        self.dK = [self._dstiff, self._dmass]
         # define the list of function to set the eigenvalue dependance of each operator matrix
         self.flda = [None, ee.lda_func.Lda]
         # ---------------------------------------------------------------------
-  
+
     # possible to add new methods
     def __repr__(self):
-        """Define the object representation.   
-        """
-        
-        return "Instance of Operator class {} @nu0={} ({} dof, height={})".format(self.__class__.__name__, self.nu0, self.n, self.h)
-        
+        """Define the object representation."""
+        return "Instance of Operator class {} @nu0={} ({} dof, height={})".format(self.__class__.__name__,
+                                                                                  self.nu0, self.n, self.h)
+
     def _mass(self):
-        """ Define the mass matrix, of 1D FEM with ordered nodes.
-        
+        """Define the mass matrix, of 1D FEM with ordered nodes.
+
         The matrix is created by converting a sparse matrix into full matrix
         The elementary matrix reads
-        Me = (Le/6) * [2  1; 1 2]        
+        Me = (Le/6) * [2  1; 1 2]
         Thus the lines are [2,1,0], [1,4,1], [0,1,2] x (Le/6)
-        """        
-        n=self.n
+        """
+        n = self.n
         # create M matrix (complex)
         # value for a inner M row [m1 m2 m1]
         m1 = self.Le / 6.
         m2 = self.Le * 4./6.
         # Interior grid points
-        M=sp.sparse.diags([m1, m2, m1], [-1, 0, 1], shape=(n, n)).toarray()
+        M = sp.sparse.diags([m1, m2, m1], [-1, 0, 1], shape=(n, n)).toarray()
         # Boundary points
-        M[0,0] = m2/2.
-        M[0,1] = m1
-        M[n-1,n-2] = m1
-        M[n-1,n-1] = m2/2.
-        
-        
+        M[0, 0] = m2/2.
+        M[0, 1] = m1
+        M[n-1, n-2] = m1
+        M[n-1, n-1] = m2/2.
+
         # store it
-        self._Mmat=M
-        
+        self._Mmat = M
+
     def _stif(self):
         """Define the stifness matrix of 1D FEM with ordered nodes.
-        
+
         The matrix is created by converting a sparse matrix into full matrix
         The elementary matrix read
         Ke = (1/Le) * [1 -1; -1 1]
@@ -168,65 +164,65 @@ class Znumpy(ee.OP):
         n = self.n
         # create K and M matrix (complex)
         # value for inner K row [k1 k2 k1]
-        k1 = -1. /self.Le
-        k2 =  2. /self.Le
-        
+        k1 = -1. / self.Le
+        k2 = 2. / self.Le
+
         # Striffness matrix
         # Interior grid points
-        K=sp.sparse.diags([k1, k2, k1], [-1, 0, 1], shape=(n, n)).toarray()  
+        K = sp.sparse.diags([k1, k2, k1], [-1, 0, 1], shape=(n, n)).toarray()
         # Boundary points
-        K[0,0] = k2/2.
-        K[0,1] = k1
-        K[n-1,n-2] = k1
-        K[n-1,n-1] = k2/2.
+        K[0, 0] = k2/2.
+        K[0, 1] = k1
+        K[n-1, n-2] = k1
+        K[n-1, n-1] = k2/2.
 
         # store it
-        self._Kmat=K
-        
+        self._Kmat = K
+
     def _gam(self):
-        """Define the Gamma matrix accounting for the impedance BC.
-        
-        Zeros everywhere except on the 1st node $\Gamma(0) = 1 $  
+        r"""Define the Gamma matrix accounting for the impedance BC.
+
+        Zeros everywhere except on the 1st node $\Gamma(0) = 1 $
         The matrix is created by converting a sparse matrix into full matrix
-        
+
         Parameters
         ----------
         z0 : complex
-            The impedance value 
+            The impedance value
         """
         n = self.n
         # create Gamma matrix (complex)
         # Striffness matrix
-        Gam= sp.sparse.coo_matrix((np.array([1.]), (np.array([0]), np.array([0]))), shape=(n, n)).toarray()
-        #store it
-        self._GamMat=Gam
-            
+        Gam = sp.sparse.coo_matrix((np.array([1.]), (np.array([0]), np.array([0]))), shape=(n, n)).toarray()
+        # Store it
+        self._GamMat = Gam
+
     def _ImpMat(self):
         """Return the operator matrices list for the generalized eigenvalue problem.
-        
+
         Returns
         -------
         K : matrix
            K contains [k0^2*M + Gamma*1i*const.rho0*omega/Z - K , -M]
         """
         omega = self.k * self.c    # angular freq.
-        Zeff =   1j * omega * self.rho / self.nu0
-        
-        K=[]
-        KK = self.k**2*self._Mmat + Zeff*self._GamMat - self._Kmat       
+        Zeff = 1j * omega * self.rho / self.nu0
+
+        K = []
+        KK = self.k**2*self._Mmat + Zeff*self._GamMat - self._Kmat
         # encapsulated into list
         K.append(KK)
         K.append(-self._Mmat)
-        
+
         return K
 
-    def _dstiff(self,n):
+    def _dstiff(self, n):
         r"""Define the sucessive derivative of the $\tilde{K}$ matrix with respect to nu.
-        
+
         L = \tilde{K}- lda M (standard FEM formalism)
         with polynomial formlism L = K0 + lda K1 + ...
         thus K0=\tilde{K}
-        
+
         Parameters
         ----------
         n : int
@@ -234,25 +230,25 @@ class Znumpy(ee.OP):
         Returns
         -------
         Kn : Matrix (petsc or else)
-            The n-derivative of global K0 matrix 
+            The n-derivative of global K0 matrix
         """
-        if n==0:
-            Kn= self.K[0]
+        if n == 0:
+            Kn = self.K[0]
         else:
             omega = self.k * self.c    # angular freq.
             # derivative of (1/Z)
-            Zn= (-1)**n *factorial(n)/self.nu0**(n+1)
-            Kn= self._GamMat * 1j*self.rho*omega*Zn
-        
+            Zn = (-1)**n * factorial(n)/self.nu0**(n+1)
+            Kn = self._GamMat * 1j*self.rho*omega*Zn
+
         return Kn
-    
-    def _dmass(self,n):
+
+    def _dmass(self, n):
         """Define the sucessive derivative of the $M$ matrix with respect to nu.
-        
+
         L = K - lda M (standard FEM formalism)
         with polynomial formlism L = K0 + lda K1 + ...
         thus K1=-M
-        
+
         Parameters
         ----------
         n : int
@@ -263,18 +259,18 @@ class Znumpy(ee.OP):
             The n-derivative of global K1 matrix
         """
         # if n=0 return M
-        if n==0:
+        if n == 0:
             return -self._Mmat
         # if n!= 0 return 0 because M is constant
-        else:            
+        else:
             return 0
 
 
 def main(N=5):
-    """ run the example
+    """Run the example.
 
     Parameters
-    -----------
+    ----------
     N : integer
         number of degree of freedom
 
@@ -285,64 +281,62 @@ def main(N=5):
     vp1 : Eig
         The object containing all information on the 1st eigenvalue
     """
-
     import numpy as np
     import time
 
     pi = np.pi
-    # Number of derivative 
-    Nderiv=12
+    # Number of derivative
+    Nderiv = 12
     # air properties
     rho0, c0 = 1.2, 340.
     # initial imepdance guess
-    #z0=400+3j
-    z0= 486.198103097114 + 397.605679264872j
+    # z0=400+3j
+    z0 = 486.198103097114 + 397.605679264872j
     # freq. of computation
     f = 200
     # modal index pair
-    n1=0
-    n2=1    
+    n1 = 0
+    n2 = 1
     # number of dof
     # N=5 -> move to function
     # duct height
     h = 1.
     # number of mode to compute
-    Nmodes=4
+    Nmodes = 4
     # solve the problem
     omega = 2*pi*f
     k0 = omega/c0
     # Create discrete operator of the pb
-    Imp=Znumpy(z=z0,n=N,h=h,rho=rho0,c=c0,k=k0)
+    Imp = Znumpy(z=z0, n=N, h=h, rho=rho0, c=c0, k=k0)
     print(Imp)
     # initialize eigenvalue solver for *generalized eigenvalue problem*
     Imp.createSolver(pb_type='gen')
     # run the eigenvalue computation
-    Lambda = Imp.solver.solve(nev=Nmodes,target=0+0j,skipsym=False)
+    Lambda = Imp.solver.solve(nev=Nmodes, target=0+0j, skipsym=False)
     # return the eigenvalue and eigenvector in a list of Eig object
-    extracted = Imp.solver.extract( [n1,n2] )
+    extracted = Imp.solver.extract([n1, n2])
     # destroy solver (important for petsc/slepc)
     Imp.solver.destroy()
     print('> Eigenvalue :', Lambda)
-
 
     print('> Get eigenvalues derivatives ...\n')
     # Create Eig object
     vp1, vp2 = extracted
 
     print('> Get derivative vp1 ...\n')
-    tic = time.time() # init timer 
-    vp1.getDerivatives(Nderiv,Imp)
-    print("              derivative real time :", time.time()-tic)  
+    tic = time.time()  # init timer
+    vp1.getDerivatives(Nderiv, Imp)
+    print("              derivative real time :", time.time()-tic)
     print(vp1.dlda)
-    tic = time.time() # init timer
-    vp2.getDerivatives(Nderiv,Imp)
-    print("              derivative real time :", time.time()-tic) 
+    tic = time.time()  # init timer
+    vp2.getDerivatives(Nderiv, Imp)
+    print("              derivative real time :", time.time()-tic)
     print(vp2.dlda)
 
-    # Locate EP 
-    tic = time.time() # init timer
+    # Locate EP
+    tic = time.time()  # init timer
     # create EP instance to find the merging of vp1 and vp2
-    EP1=ee.EP(vp1,vp2)
+    EP1 = ee.EP(vp1, vp2)
     loc = EP1.locate()[0]
 
     print('\n> EP location :')
@@ -355,7 +349,8 @@ def main(N=5):
 
     return EP1, vp1
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     """Show graphical outputs and reconstruction examples.
     """
     import numpy as np
@@ -366,17 +361,17 @@ if __name__=='__main__':
     EP1.plotZeros()
 
     # compute reconstruction
-    N=5 # limit series order to 5 terms
-    points = np.linspace(-125,125,101) + EP1.nu0
-    tay = vp1.taylor(points,n=N)
-    pad = vp1.pade(points,n=N)
-    pui1,pui2 = vp1.puiseux(EP1,points,n=N)
-    ana1,ana2 = vp1.anaAuxFunc(EP1,points,n=N)
+    N = 5  # limit series order to 5 terms
+    points = np.linspace(-125, 125, 101) + EP1.nu0
+    tay = vp1.taylor(points, n=N)
+    pad = vp1.pade(points, n=N)
+    pui1, pui2 = vp1.puiseux(EP1, points, n=N)
+    ana1, ana2 = vp1.anaAuxFunc(EP1, points, n=N)
 
     # plotting
     plt.figure()
-    plt.plot(points.real,tay.real,'k',label='Taylor series')
-    plt.plot(points.real,pui2.real,'b',label='Puiseux series') # converge well if N=10
-    plt.plot(points.real,pad.real,'teal',label='Padé appox.')
-    plt.plot(points.real,ana2.real,'g.-',label='AAF approx')
+    plt.plot(points.real, tay.real, 'k', label='Taylor series')
+    plt.plot(points.real, pui2.real, 'b', label='Puiseux series')  # converge well if N=10
+    plt.plot(points.real, pad.real, 'teal', label='Padé appox.')
+    plt.plot(points.real, ana2.real, 'g.-', label='AAF approx')
     plt.legend()
