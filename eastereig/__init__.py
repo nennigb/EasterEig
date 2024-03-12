1cfa67eb45ae2ff6dcefd0b24cf41f1ed4403e73
bn <benoit.nennig@isae-supmeca.fr>
2024-03-12 11:38:33 +0100

    Remove fpoly from top namespace in __init__.
      - Do not load all importlib module.

--- eastereig/__init__.py
+++ eastereig/__init__.py
@@ -31,23 +31,23 @@ EasterEig  -- A library to locate exceptional points and to reconstruct eigenval
 .. image::../../figures/packages.svg
 
 """
-import importlib as _importlib
+from importlib.util import find_spec as _find_spec
 import numpy as _np
 import os
 
 # usefull also to pdoc
-__all__= ['OP','Eig','EP','Loci','gopts']
+__all__ = ['OP', 'Eig', 'EP', 'Loci', 'gopts']
 
 # check if petsc4py and slepc4py are installed
-if _importlib.util.find_spec('petsc4py'):
-    if _importlib.util.find_spec('slepc4py'):
-        _petscHere=True
+if _find_spec('petsc4py'):
+    if _find_spec('slepc4py'):
+        _petscHere = True
 else:
-    _petscHere=False
+    _petscHere = False
 
-# link to version
+
 def _getversion():
-    """ Get version from VERSION."""
+    """Get version from VERSION."""
     v = None
     with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
         for line in f:
@@ -56,28 +56,24 @@ def _getversion():
                 break
         return v
 
+
 __version__ = _getversion()
 
 # define usefull constante
-_CONST=_np.array([4.715922776012983e+257+2.3562408023262842e+251j,
-       9.076523811470737e+223+1.1063884891110190e+200j,
-       3.056759640868045e-086+6.0134700169916254e-154j,
-       5.981496655985495e-154+1.0357232245733592e-013j,
-       6.013344832270628e-154+4.4759381596057782e-091j,
-       6.013469533206161e-154+1.4143587736555866e+190j,
-       6.013470015120088e-154+4.4759381595361591e-091j,
-       6.013470016991616e-154+9.5500162705244384e-260j]).tobytes()
+_CONST = _np.array([4.715922776012983e+257+2.3562408023262842e+251j,
+                    9.076523811470737e+223+1.1063884891110190e+200j,
+                    3.056759640868045e-086+6.0134700169916254e-154j,
+                    5.981496655985495e-154+1.0357232245733592e-013j,
+                    6.013344832270628e-154+4.4759381596057782e-091j,
+                    6.013469533206161e-154+1.4143587736555866e+190j,
+                    6.013470015120088e-154+4.4759381595361591e-091j,
+                    6.013470016991616e-154+9.5500162705244384e-260j]).tobytes()
 
 
-# import class
+# Import class
 from .options import gopts
 from .eig import Eig
 from .op import OP
 from .ep import EP
 from .loci import Loci
 from . import lda_func
-from .fpoly import fpoly
-from .charpol import CharPol
-
-
-
