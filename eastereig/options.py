c7329a24bd098783baa064fc443ccfd2a72c58aa
bn <benoit.nennig@isae-supmeca.fr>
2024-10-24 15:11:01 +0200

    Update options with 'max_workers_mult' used in charpol multiply.

--- eastereig/options.py
+++ eastereig/options.py
@@ -16,17 +16,20 @@
 # You should have received a copy of the GNU General Public License
 # along with Eastereig.  If not, see <https://www.gnu.org/licenses/>.
 """
-This file is use to set default value of eastereig options. Most of these options
-concerns petsc and slepc solvers (for the moment).
+This file is used to set default values of eastereig options. Most of these options
+concerns petsc/slepc solvers or charpol.
 
-  1. For the momment, only `mumps` direct solver has been used. 
-  2. Sometimes mumps crash with high fill. The problem arise if the prediction/mem 
-  allocation is too different (default 20%) from the real computation. 
+  1. For the momment, only `mumps` direct solver has been used.
+  2. Sometimes mumps crash with high fill. The problem arise if the prediction/mem
+  allocation is too different (default 20%) from the real computation.
   Setting 'icntl_14'=50, fix the problem.
 
 """
 
-gopts ={'direct_solver_name':'mumps',                       # petsc name of the direct solver
-       'direct_solver_petsc_options_name':'mat_mumps_',     # petsc direct solver name of in `PETSc.Options`
-       'direct_solver_petsc_options_dict':{'icntl_14':50},  # dictionnary of the petsc options name, value
-       }
+gopts = {'direct_solver_name': 'mumps',                         # petsc name of the direct solver
+         'direct_solver_petsc_options_name': 'mat_mumps_',      # petsc direct solver name of in `PETSc.Options`
+         'direct_solver_petsc_options_dict': {'icntl_14': 50},  # dictionnary of the petsc options name, value
+         # Number of workers in charpol multiply. Set to the number of cores for speed,
+         # set to 1 for more compatibility if mixed with petsc matrices
+         'max_workers_mult': 1,
+         }
